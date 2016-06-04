fc29946ec72aac3d6dd4575ddb7ceb22647da5ee
Guilherme Santos <ggs12@inf.ufpr.br>
2016-06-04 13:46:23 -0300

    xml parsing working

--- js/t4-visualizeitor.js
+++ js/t4-visualizeitor.js
@@ -1,46 +1,75 @@
 "use strict";
 
-function Aluno(XMLdata){
-	this.grr = XMLdata.getElementsByTagName("MATR_ALUNO")[0].firstChild.nodeValue;
-    this.nome  = XMLdata.getElementsByTagName("NOME_ALUNO")[0].firstChild.nodeValue;
-    this.materia   = XMLdata.getElementsByTagName("NOME_ATIV_CURRIC")[0].firstChild.nodeValue;
-    this.codMateria    = XMLdata.getElementsByTagName("COD_ATIV_CURRIC")[0].firstChild.nodeValue;
-    if((XMLdata.getElementsByTagName("FREQUENCIA")[0].firstChild != null) && (XMLdata.getElementsByTagName("SIGLA")[0].firstChild != null)) {
-	    this.ano        = XMLdata.getElementsByTagName("ANO")[0].firstChild.nodeValue;
-	    this.periodo    = XMLdata.getElementsByTagName("PERIODO")[0].firstChild.nodeValue;
-	    this.nota       = XMLdata.getElementsByTagName("MEDIA_FINAL")[0].firstChild.nodeValue;
-	    this.frequencia = XMLdata.getElementsByTagName("FREQUENCIA")[0].firstChild.nodeValue;
-	    this.situacao   = XMLdata.getElementsByTagName("SIGLA")[0].firstChild.nodeValue;
-	    this.tipo	   = XMLdata.getElementsByTagName("DESCR_ESTRUTURA")[0].firstChild.nodeValue;
-    }
+function main(){
+       $.ajax({
+	        type: "GET" ,
+	        url: "../alunos.xml" ,
+	        dataType: "xml" ,
+	        success: function(xml) {
+	        	AlunosParser(xml);
+        	}
+   		});
+
+    $('#buscar').click(function(e){
+    	e.preventDefault();
+    	montaGrade($('#grr-aluno').val());
+    });
 }
 
+function Aluno(){
+	this.grr = null;
+    this.nome  = null;
+    this.materia   = null;
+    this.codMateria    = null;
+    this.ano        = null;
+    this.periodo    = null;
+    this.nota       = null;
+    this.frequencia = null;
+    this.situacao   = null;
+    this.tipo	   = null;
+}
 
-function XMLparser(xml){
-	var XMLalunos = xml.getElementsByTagName("ALUNO");
-	for (var i = 0; i < alunos.length; i++) {
-		var aluno = new Aluno(XMLalunos);
+function AlunosParser(xml){
+	var $xml  = $(xml);
+	var $alunos = $xml.find('ALUNO');
+	$alunos.each(function(){
+		var aluno = new Aluno();
+		aluno.grr = $(this).find('MATR_ALUNO').text();
+		aluno.nome = $(this).find('NOME_ALUNO').text();
+		aluno.materia = $(this).find('NOME_ATIV_CURRIC').text();
+		aluno.codMateria = $(this).find('COD_ATIV_CURRIC').text();
+		aluno.frequencia = $(this).find('FREQUENCIA').text();
+		aluno.situacao = $(this).find('SIGLA').text();
+		if (aluno.frequencia != null && aluno.situacao != null) {
+			aluno.ano = $(this).find('ANO').text();
+			aluno.periodo = $(this).find('PERIODO').text();
+			aluno.nota = $(this).find('MEDIA_FINAL').text();
+			aluno.tipo = $(this).find('DESCR_ESTRUTURA').text();
+		}
 		alunosParsed.push(aluno);
-	}
-	return alunosParsed;
+	});
 }
+
+function findMaterias(grr){
+	var $alunos = $(alunosParsed);
+	var aluno = [];
+	$alunos.each(function(){
+		if (this.grr == grr) {
+			aluno.push(this);
+		}
+	});
+	return aluno;
+}
+
+function montaGrade(grr-aluno){
+	var materias = findMaterias(grr-aluno);
+	
+}
+
 
 var alunosParsed =  [];
-
 $(function(){
-	function main() {
-	    $.get("alunos.xml", function(data){
-	        alunosParsed = XMLparser(data);
-	        a = new Aluno(alunosParsed);
-	    });
-
-	    $('#buscar').click(function(){
-
-	    });
-	}
-
 	main();
-
 });
 
 
